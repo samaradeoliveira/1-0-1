f8674936e634212df7c7ae69a65262df48ddee8d
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-05-24 15:49:09 -0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,26 @@
+# importe o módulo time
+
+  
+# defina a função de contagem regressiva do cronômetro
+
+    #criar um laço de repetição while
+ 
+        #duas variáveis para guardar minutos e segundos
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+        
+        #para printar na tela o cronomêtro
+        timer = f'{mins}:{secs}'
+        print(timer)
+        
+        #subtrair segundos
+        seconds -= 1
+    
+    #printar uma mensagem para o tester
+    
+  
+  
+# input time in seconds
+
+  
+# function call
